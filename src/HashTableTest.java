3db107a56f3df45b3801e5ba35b59f6cd50cd688
Sawyer Ball <sawyerball@u.boisestate.edu>
2021-11-02 19:15:34 -0600

    Twin Prime Generator Tests

--- src/HashTableTest.java
+++ src/HashTableTest.java
@@ -1,5 +1,12 @@
 public class HashTableTest {
+
+
     public static void main(String[] args) {
+
+        TwinPrimeGenerator twinPrimeGen = new TwinPrimeGenerator();
+
+        System.out.println("HashtableTest: Twin prime table size found in the range [95500..96000]: "
+                + twinPrimeGen.twinPrimeGenerator(95500, 96000));
 
     }
 }
